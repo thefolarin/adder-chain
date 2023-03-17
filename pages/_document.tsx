a85b0c536a4b3d4258a06eaedd9ba62249deb1da
thefolarin <105429512+thefolarin@users.noreply.github.com>
2023-03-17 13:19:18 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,9 @@ export default function Document() {
     <Html lang='en'>
       <Head>
         <meta name='description' content='WC Live app' />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <link
           href='https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap'
           rel='stylesheet'
